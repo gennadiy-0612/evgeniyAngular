ac5ca9fb55d9562b20482cbd3af7dc2a8b721f6f
Genn <intval@i.ua>
2021-08-13 10:29:03 +0300

    add-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RentComponent } from './rent/rent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentComponent
   ],
   imports: [
     BrowserModule
