1e646f3638eda5c05219298b1c350af8a05c8045
Genn <intval@i.ua>
2021-08-14 15:47:06 +0300

    add-header

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -35,53 +35,53 @@
 //     });
 // }
 function bugIE(base, added) {
-    let images = new Image()
-    images.hasWebP = true;
-    images.src = './assets/img/car.webp';
-    images.onerror = function () {
-        images.hasWebP = false;
-        images.setting();
-        tryyy();
-    };
-    images.setting = function () {
-        document.querySelector(base).setAttribute('class', document.querySelector(base).getAttribute('class') + ' ' + added);
-    }
+  let images = new Image()
+  images.hasWebP = true;
+  images.src = './assets/img/car.webp';
+  images.onerror = function () {
+    images.hasWebP = false;
+    images.setting();
+    tryyy();
+  };
+  images.setting = function () {
+    document.querySelector(base).setAttribute('class', document.querySelector(base).getAttribute('class') + ' ' + added);
+  }
 }
 
 bugIE('p.image', 'car-webp1 bug webp fix');
 bugIE('p.image', 'car-webp1 bug webp fix');
 
 function tryyy() {
-    console.log('ooo');
+  console.log('ooo');
 }
 
 let cacheAuto = {
-    show: 'techno mech show',
-    hide: 'techno mech hide',
-    linkActive: 'kind mechanic active',
-    linkNotActive: 'kind mechanic not-active',
-    oldTag: document.querySelectorAll('.techno')[0],
-    oldTagTwo: document.querySelectorAll('.techno')[1],
-    oldLink: document.querySelectorAll('.kind')[0],
-    oldLinkTwo: document.querySelectorAll('.kind')[1]
+  show: 'techno mech show',
+  hide: 'techno mech hide',
+  linkActive: 'kind mechanic active',
+  linkNotActive: 'kind mechanic not-active',
+  oldTag: document.querySelectorAll('.techno')[0],
+  oldTagTwo: document.querySelectorAll('.techno')[1],
+  oldLink: document.querySelectorAll('.kind')[0],
+  oldLinkTwo: document.querySelectorAll('.kind')[1]
 }
 
 function switching() {
-    cacheAuto.cachingClass = cacheAuto.oldTag.classList[2];
-    if (this.classList[2] === 'active') {
-        return true;
-    }
-    if (cacheAuto.cachingClass === 'show') {
-        cacheAuto.oldTag.setAttribute('class', cacheAuto.hide);
-        cacheAuto.oldTagTwo.setAttribute('class', cacheAuto.show);
-        cacheAuto.oldLink.setAttribute('class', cacheAuto.linkNotActive);
-        cacheAuto.oldLinkTwo.setAttribute('class', cacheAuto.linkActive);
-    } else {
-        cacheAuto.oldTagTwo.setAttribute('class', cacheAuto.hide);
-        cacheAuto.oldLinkTwo.setAttribute('class', cacheAuto.linkNotActive);
-        cacheAuto.oldTag.setAttribute('class', cacheAuto.show);
-        cacheAuto.oldLink.setAttribute('class', cacheAuto.linkActive);
-    }
+  cacheAuto.cachingClass = cacheAuto.oldTag.classList[2];
+  if (this.classList[2] === 'active') {
+    return true;
+  }
+  if (cacheAuto.cachingClass === 'show') {
+    cacheAuto.oldTag.setAttribute('class', cacheAuto.hide);
+    cacheAuto.oldTagTwo.setAttribute('class', cacheAuto.show);
+    cacheAuto.oldLink.setAttribute('class', cacheAuto.linkNotActive);
+    cacheAuto.oldLinkTwo.setAttribute('class', cacheAuto.linkActive);
+  } else {
+    cacheAuto.oldTagTwo.setAttribute('class', cacheAuto.hide);
+    cacheAuto.oldLinkTwo.setAttribute('class', cacheAuto.linkNotActive);
+    cacheAuto.oldTag.setAttribute('class', cacheAuto.show);
+    cacheAuto.oldLink.setAttribute('class', cacheAuto.linkActive);
+  }
 }
 
 cacheAuto.listSlide = document.querySelectorAll('.slider');
@@ -89,67 +89,67 @@ cacheAuto.listSlideStart = 0;
 cacheAuto.listSlideCurrent = 0;
 cacheAuto.listSlideAll = cacheAuto.listSlide.length;
 cacheAuto.backGo = function (event) {
-    event.preventDefault();
-    if (cacheAuto.listSlideCurrent !== cacheAuto.listSlideStart) {
-        cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider');
-        document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number');
-        cacheAuto.listSlideCurrent--;
-        cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider show');
-        document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number active');
-    }
-}
-cacheAuto.forwardGo = function (event) {
-    event.preventDefault();
-    if (cacheAuto.listSlideAll === cacheAuto.listSlideCurrent + 1) {
-        return true;
-    }
-    if (cacheAuto.listSlideAll !== cacheAuto.listSlideCurrent) {
-        cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider');
-        document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number');
-        cacheAuto.listSlideCurrent++;
-        cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider show');
-        document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number active');
-    }
-};
-cacheAuto.setEmotion = function () {
+  event.preventDefault();
+  if (cacheAuto.listSlideCurrent !== cacheAuto.listSlideStart) {
     cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider');
     document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number');
-    cacheAuto.listSlide[this.textContent - 1].setAttribute('class', 'slider show');
-    cacheAuto.listSlideCurrent = this.textContent - 1;
+    cacheAuto.listSlideCurrent--;
+    cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider show');
     document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number active');
+  }
 }
-document.querySelector('.kind.mechanic').addEventListener("click", switching, false);
-document.querySelector('.kind.automate').addEventListener("click", switching, false);
-document.querySelector('.back-go').addEventListener("click", cacheAuto.backGo, false);
-document.querySelector('.forward-go').addEventListener("click", cacheAuto.forwardGo, false);
+cacheAuto.forwardGo = function (event) {
+  event.preventDefault();
+  if (cacheAuto.listSlideAll === cacheAuto.listSlideCurrent + 1) {
+    return true;
+  }
+  if (cacheAuto.listSlideAll !== cacheAuto.listSlideCurrent) {
+    cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider');
+    document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number');
+    cacheAuto.listSlideCurrent++;
+    cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider show');
+    document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number active');
+  }
+};
+cacheAuto.setEmotion = function () {
+  cacheAuto.listSlide[cacheAuto.listSlideCurrent].setAttribute('class', 'slider');
+  document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number');
+  cacheAuto.listSlide[this.textContent - 1].setAttribute('class', 'slider show');
+  cacheAuto.listSlideCurrent = this.textContent - 1;
+  document.querySelectorAll('.emotion-number')[cacheAuto.listSlideCurrent].setAttribute('class', 'emotion-number active');
+}
+//document.querySelector('.kind.mechanic').addEventListener("click", switching, false);
+//document.querySelector('.kind.automate').addEventListener("click", switching, false);
+//document.querySelector('.back-go').addEventListener("click", cacheAuto.backGo, false);
+//document.querySelector('.forward-go').addEventListener("click", cacheAuto.forwardGo, false);
 cacheAuto.setEmotion.allEl = document.querySelectorAll('.emotion-number');
 for (let i = 0; i < cacheAuto.setEmotion.allEl.length; i++) {
-    cacheAuto.setEmotion.allEl[i].addEventListener("click", cacheAuto.setEmotion, false);
+  cacheAuto.setEmotion.allEl[i].addEventListener("click", cacheAuto.setEmotion, false);
 }
 
 function ScrollDetect(whoIsAnimate, whatKindAnimate) {
-    this.elementTarget = whoIsAnimate;
-    this.animateClass = whatKindAnimate;
-    this.windowH = window.innerHeight;
-    this.startChanges = 0;
-    this.initProps = function () {
-        if (this.startChanges === 0) {
-            this.elementWork = document.querySelector(this.elementTarget);
-            this.initClass = this.elementWork.getAttribute('class');
-            this.elPositonY = this.elementWork.offsetTop;
-            this.startChanges = 1;
-        }
-    };
-    this.scrolling = function () {
-        this.initProps();
-        if ((this.elPositonY + 100) > Math.floor(window.scrollY) && Math.floor(window.scrollY) > (this.elPositonY - this.windowH)) {
-            this.elementWork.setAttribute('class', this.initClass + ' ' + this.animateClass);
-            console.clear();
-            console.log(this.elementWork);
-        } else {
-            this.elementWork.setAttribute('class', this.initClass);
-        }
+  this.elementTarget = whoIsAnimate;
+  this.animateClass = whatKindAnimate;
+  this.windowH = window.innerHeight;
+  this.startChanges = 0;
+  this.initProps = function () {
+    if (this.startChanges === 0) {
+      this.elementWork = document.querySelector(this.elementTarget);
+      this.initClass = this.elementWork.getAttribute('class');
+      this.elPositonY = this.elementWork.offsetTop;
+      this.startChanges = 1;
     }
+  };
+  this.scrolling = function () {
+    this.initProps();
+    if ((this.elPositonY + 100) > Math.floor(window.scrollY) && Math.floor(window.scrollY) > (this.elPositonY - this.windowH)) {
+      this.elementWork.setAttribute('class', this.initClass + ' ' + this.animateClass);
+      console.clear();
+      console.log(this.elementWork);
+    } else {
+      this.elementWork.setAttribute('class', this.initClass);
+    }
+  }
 }
 
 ScrollDetect.firstEffect = new ScrollDetect('.sub.title', 'appear');
